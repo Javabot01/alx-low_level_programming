b9c5ec8958421346ab3be2ea7aa9751d5718a829
Javabot01 <davidoyebode01@gmail.com>
2023-04-04 20:08:00 -0700

    MEMSET

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory
- * area pointed to by s with the constant byte b
- * @s: pointer to memory area
- * @b: constant byte
- * @n: number of bytes
- * Return: pointer to the memory area s
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int x;
-	
-	for (x = 0; x < n; ++x)
-		*(s + x) = b;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 	return (s);
 }
